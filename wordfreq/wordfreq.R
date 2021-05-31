a162af2cadad833dc901d2f1ca2e435b2ff777a3
andreashih <r08142004@ntu.edu.tw>
2021-05-31 12:19:22 +0800

    Update wordfreq.R

--- wordfreq/wordfreq.R
+++ wordfreq/wordfreq.R
@@ -5,24 +5,24 @@ setwd("../data/time_sliced_collapsed")
 
 ## read files
 all_files <- list.files()
-all_files_content <- lapply(all_files, function(x)(readLines(x, encoding = 'UTF-8')))
 
 count_freq <- function(article){
-  # preprocessing
-  words <- unlist(strsplit(article, '\u3000'))
-  cleaned_words <- str_replace_all(words, '[^[:alnum:]]', '') # remove special characters
-  cleaned_words <- str_replace_all(cleaned_words, '[A-Za-z0-9]', '') # remove eng and digits
+  # read file
+  article_name <- str_remove(article, '.txt')
+  content <- readLines(article, encoding = 'UTF-8')
+  words <- unlist(strsplit(content, '\u3000'))
   
   # word freq
-  words_freq<-table(unlist(cleaned_words))
-  num_of_tokens <- length(unique(cleaned_words))
+  words_freq<-table(unlist(words))
+  num_of_tokens <- length(unique(words))
   
   # build word freq df
   words_freq_df <- as.data.frame(cbind(names(words_freq),
                                        as.integer(words_freq)/num_of_tokens))
   colnames(words_freq_df) <- c('word', 'freq')
+  words_freq_df$src <- strsplit(article_name, '_')[[1]][1]
+  words_freq_df$timestamp <- strsplit(article_name, '_')[[1]][2]
   return(words_freq_df)
 }
 
-## pass the function to all files content
-all_files_content_freq <- lapply(all_files_content, function(x)(count_freq(x)))
+all_files_freq <- bind_rows(lapply(all_files, function(x)(count_freq(x))))

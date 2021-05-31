a02869e5f86f50d8575f6058b5a6ad039682c91c
andreashih <r08142004@ntu.edu.tw>
2021-05-31 12:42:00 +0800

    Update wordfreq.R

--- wordfreq/wordfreq.R
+++ wordfreq/wordfreq.R
@@ -1,28 +1,44 @@
 library(readr)
 library(stringr)
+library(dplyr)
+library(ggplot2)
 
 setwd("../data/time_sliced_collapsed")
 
-## read files
-all_files <- list.files()
-
-count_freq <- function(article){
-  # read file
-  article_name <- str_remove(article, '.txt')
-  content <- readLines(article, encoding = 'UTF-8')
-  words <- unlist(strsplit(content, '\u3000'))
+freq_viz <- function(words){
   
-  # word freq
-  words_freq<-table(unlist(words))
-  num_of_tokens <- length(unique(words))
+  all_files <- list.files('.', '.*.txt')
   
-  # build word freq df
-  words_freq_df <- as.data.frame(cbind(names(words_freq),
-                                       as.integer(words_freq)/num_of_tokens))
-  colnames(words_freq_df) <- c('word', 'freq')
-  words_freq_df$src <- strsplit(article_name, '_')[[1]][1]
-  words_freq_df$timestamp <- strsplit(article_name, '_')[[1]][2]
-  return(words_freq_df)
+  count_freq <- function(article){
+    # read file
+    article_name <- str_remove(article, '.txt')
+    content <- readLines(article, encoding = 'UTF-8')
+    words <- unlist(strsplit(content, '\u3000'))
+    
+    # word freq
+    words_freq<-table(unlist(words))
+    num_of_tokens <- length(unique(words))
+    
+    # build word freq df
+    words_freq_df <- as.data.frame(cbind(names(words_freq),
+                                         as.integer(words_freq)/num_of_tokens))
+    colnames(words_freq_df) <- c('word', 'freq')
+    words_freq_df$src <- strsplit(article_name, '_')[[1]][1]
+    words_freq_df$timestamp <- strsplit(article_name, '_')[[1]][2]
+    return(words_freq_df)
+  }
+  
+  all_files_freq <- bind_rows(lapply(all_files, function(x)(count_freq(x))))
+  
+  # filter matched words' data
+  matched <- all_files_freq %>%
+    filter(word %in% words)
+  
+  # visualization
+  p <- ggplot(data=matched) +
+    geom_bar(mapping = aes(x = timestamp, y = freq, fill = src),
+             stat = 'identity', position = "dodge") +
+    facet_wrap(vars(word))
+  
+  return(p)
 }
-
-all_files_freq <- bind_rows(lapply(all_files, function(x)(count_freq(x))))

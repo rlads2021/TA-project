fae71eaabfc9540588a0fbb27b540f91a526adc6
diff94 <62204022+diff94@users.noreply.github.com>
2021-06-08 16:04:43 +0800

    revised

--- topic_modeling/topic_viz2.R
+++ topic_modeling/topic_viz2.R
@@ -13,12 +13,13 @@ library(tidyverse)
 library(tidyr)
 library(quanteda)
 doc_dtm_group<-readRDS(file = "/doc_dtm_group.rds")
+searchterms<- readRDS("/searchterms.rds")
 top5termsPerTopic<-readRDS("/top5termsPerTopic.rds")
 topicNames<- readRDS("/topicNames.rds")
 theta<-readRDS("/theta.rds")
-#本機上這樣跑比較好看
-topic_viz <- function(word,topicThreshold,sources){
-  topicToFilter <- grep(word, topicNames)  
+
+topic_viz_2 <- function(word,topicThreshold,sources){
+  topicToFilter <- grep(word, searchterms) #search terms是所有文章資料有斷出的詞彙來找
   topicThreshold <- 0.2
   selected <- list()
   for (i in seq(1, length(topicToFilter))){
@@ -28,10 +29,13 @@ topic_viz <- function(word,topicThreshold,sources){
   #exampleIds <- selectedDocumentIndexes
   exampleIds <- unique(unlist(selected))  
   K = length(topicNames [topicToFilter])  
+  #doc_dtm$time <- substr(doc_dtm$doc_id,nchar(doc_dtm$doc_id),nchar(doc_dtm$doc_id)) PTT這邊已經有惹 time 了不用再做 
+  # get mean topic proportions per decade
+  
   sources_filter <- doc_dtm_group[exampleIds,] %>% filter(group ==sources) 
   
   topic_proportion_per_time <- aggregate(theta[sources_filter$ID,topicToFilter], by = list(time = sources_filter$time), mean)
-        
+            
   # set topic names to aggregated columns
   colnames(topic_proportion_per_time)[2:(K+1)] <- topicNames [topicToFilter]  
   # reshape data frame
@@ -41,9 +45,11 @@ topic_viz <- function(word,topicThreshold,sources){
   options(repr.plot.width=30, repr.plot.height=20)
   showtext_auto()
   p<- ggplot(vizDataFrame, aes(x=time, y=value, colour = variable, group = variable)) + 
-    geom_point() + geom_line(size = 3)  
+    geom_point() + geom_line(size = 3) 
     
+  
   
   
   return(p)
 }
+

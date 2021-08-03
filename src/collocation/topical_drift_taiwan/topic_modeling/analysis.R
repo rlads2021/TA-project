d022599a01c328b68ddfce0ce6ca0c8f95eeeadb
liao <liao961120@gmail.com>
2021-08-03 15:12:29 +0800

    update

--- src/collocation/topical_drift_taiwan/topic_modeling/analysis.R
+++ src/collocation/topical_drift_taiwan/topic_modeling/analysis.R
@@ -34,72 +34,93 @@ d <- tp_left %>%
     summarise(count = sum(count)) %>%
     ungroup()
 
-d <- d %>% filter(src == "ptt")
-terms <- d$collo
-distr <- sapply(seq_along(terms), function(i) {
-        beta_vec(terms[i])
-    }) %>%
-    apply(1, mean)
+distr <- sapply(c("ptt", "weibo"), function(source) {
+  d2 <- d %>% filter(src == source)
+  terms <- d2$collo
+  distr <- sapply(seq_along(terms), function(i) {
+          d2$count[i] * beta_vec(terms[i])
+      }) %>%
+      apply(1, function(col) sum(col) / sum(d2$count))
+  return(normalize(distr))
+})
 
-
- # filter(!cluster %in% c(-1, -2)) %>%
+distr %>% 
+  as_tibble() %>% 
+  mutate(topic = factor(seq_along(ptt), order = T)) %>%
+  gather(key = "src", value = "y", ptt, weibo) %>%
 ggplot() +
-  geom_bar(aes(cluster, count/total_count, fill = src),
+  geom_bar(aes(topic, y, fill = src),
            stat = "identity", position = "dodge") +
-  scale_y_continuous(labels = scales::percent) +
   facet_wrap(vars(src), nrow = 2) +
   scale_fill_manual(values = c("ptt" = "#00BFC4", "weibo" = "#F8766D")) +
   theme(legend.position = "none") +
-  labs(y = "Percent", 
+  labs(y = "average", 
        title = "Topic contrast of “臺灣” between PTT & Weibo")
+
 
 #' ## Topic drift across time (PTT)
 # scales::hue_pal()(4)
-tp_left %>% 
-  filter(src == "ptt", !cluster %in% c(-1, -2)) %>%
+get_distr <- function(source, ts, pos = "left") {
+  d <- collo %>% 
+    filter(collo_pos %in% pos, 
+           timestep %in% ts,
+           src == source) %>%
+    group_by(collo) %>%
+    summarise(count = sum(count)) %>%
+    ungroup()
+  
+  terms <- d$collo
+  distr <- sapply(seq_along(terms), function(i) {
+          d$count[i] * beta_vec(terms[i])
+      }) %>%
+      apply(1, function(col) sum(col) / sum(d$count))
+  return(normalize(distr))
+}
+
+ptt_ts_distr <- sapply(1:9, function(ts) 
+  get_distr("ptt", ts, "left"))
+
+ptt_ts_distr %>%
+  as_tibble() %>%
+  mutate(topic = factor(seq_along(V1), order = T)) %>%
+  gather(key = "timestep", value = "y", -topic) %>%
+  mutate(timestep = gsub("V", "", timestep, fixed = T)) %>%
+  mutate(timestep = factor(as.integer(timestep, order = T))) %>%
   ggplot() +
-  geom_bar(aes(cluster, count/total_count), 
+  geom_bar(aes(topic, y), 
            stat = "identity",
            fill = "#00BFC4") +
   facet_wrap(vars(timestep)) +
-  scale_y_continuous(labels = scales::percent) +
-  labs(y = "Percent", 
+  labs(y = "average", 
        title = "Topic drift of “臺灣” across time (PTT)")
 
 #' ## Topic drift across time (Weibo)
-tp_left %>% 
-  filter(src == "weibo", !cluster %in% c(-1, -2)) %>%
+wei_ts_distr <- sapply(1:9, function(ts) 
+  get_distr("weibo", ts, "left"))
+
+wei_ts_distr %>%
+  as_tibble() %>%
+  mutate(topic = factor(seq_along(V1), order = T)) %>%
+  gather(key = "timestep", value = "y", -topic) %>%
+  mutate(timestep = gsub("V", "", timestep, fixed = T)) %>%
+  mutate(timestep = factor(as.integer(timestep, order = T))) %>%
   ggplot() +
-  geom_bar(aes(cluster, count/total_count), 
+  geom_bar(aes(topic, y), 
            stat = "identity",
            fill = "#F8766D") +
   facet_wrap(vars(timestep)) +
-  scale_y_continuous(labels = scales::percent) +
-  labs(y = "Percent", 
+  labs(y = "average", 
        title = "Topic drift of “臺灣” across time (Weibo)")
 
 
 #' # Topic variation across time
-vectorize_topic_distr <- function(source, ts) {
-  vec <- tp_left %>% 
-    filter(timestep == ts, src == source) %>% 
-    select(cluster, count)
-  
-  clusters <- as.character(tp_left$cluster) %>% as.integer()
-  for (clust in min(clusters):max(clusters)) {
-    if (!clust %in% vec$cluster) vec <- rbind(vec, c(clust, 0))
-  }
-  
-  vec <- vec %>% arrange(cluster)
-  return(vec$count)
-}
 euc_dist <- function(x, y) sqrt(sum((x - y)^2))
 cos_dist <- function(x, y) 1 - (x %*% y) / sqrt(sum(x^2) * sum(y^2)) 
 cos_sim <- function(x, y) (x %*% y) / sqrt(sum(x^2) * sum(y^2)) 
 
 #' ## Within src
 topic_dist <- sapply(c("ptt", "weibo"), function(src) {
-    topic_distr <- sapply(1:9, function(ts) vectorize_topic_distr(src, ts))
+    topic_distr <- sapply(1:9, function(ts) get_distr(src, ts, "left"))
     #mean_distr <- rep(mean(topic_distr[, 1]), nrow(topic_distr))
     mean_distr <- apply(topic_distr, 1, mean)
     topic_dist <- apply(topic_distr, 2, function(col) {
@@ -120,8 +141,8 @@ ggplot() +
 
 #' ## Between src
 topic_distr_contrast <- sapply(1:9, function(ts) {
-  ptt <- vectorize_topic_distr("ptt", ts)
-  wei <- vectorize_topic_distr("weibo", ts)
+  ptt <- get_distr("ptt", ts, "left")
+  wei <- get_distr("weibo", ts, "left")
   cos_sim(ptt, wei)
 })
 
